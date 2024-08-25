d4b4b10b0a613541ff62194ac256dfe31cfbd076
noamhaimkere <137314547+noamhaimkere@users.noreply.github.com>
2024-08-25 15:22:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const cors = require("cors");
+
+const { routesInit } = require("./routes/config_routes")
+// require("./db/mongoconnect");
+
+const app = express();
+
+// נותן גישה לכל הדומיינים לגשת לשרת שלנו
+app.use(cors());
+// כדי שנוכל לקבל באדי ויתרגם אותו לפורמט ג'ייסון
+app.use(express.json());
+// הגדרת תקיית הפאבליק כתקייה ראשית
+app.use(express.static(path.join(__dirname, "public")))
+
+routesInit(app);
+
+const server = http.createServer(app);
+
+let port = process.env.PORT || 3002
+server.listen(port);
+
+העתק קוד
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PhoneAppGUI extends JFrame {
+
+
+
+
+    private JLabel label;
+
+
+    private JButton button;
+
+
+
+
+    public PhoneAppGUI() {
+
